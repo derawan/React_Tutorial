57cb3c914d99b8727de9e1a295c9c58a1dd2ec77
irfansjah <irfansjah@gmail.com>
2020-12-10 18:17:54 +0700

    new tutorial

--- latihan01/src/mydiv2.js
+++ latihan01/src/mydiv2.js
@@ -1,10 +1,16 @@
+import {Component} from 'react';
+
+function Test()
+{
+    alert('hallo')
+}
 
 
 function BuatParagraph(prop)
 {
 
     return (
-        <p>
+        <p onClick={Test}>
             {prop.nama}
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non sit aperiam pariatur quis totam nihil quas impedit saepe repellendus, distinctio voluptatibus consectetur vel ipsa quibusdam sequi soluta blanditiis perspiciatis corporis.
         </p>
@@ -13,4 +19,16 @@ function BuatParagraph(prop)
 
 }
 
-export default BuatParagraph;
+class MyClass extends Component {
+
+    // constructor (props) {
+    //     super(props);
+    // }
+    render() {
+        return (
+        <div>alfa = {this.props.name}</div>
+        )
+    }
+}
+
+export {BuatParagraph,MyClass};
