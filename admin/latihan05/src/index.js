c25ff54e42818c08117bac97fd28b8f89e012c56
irfansjah <irfansjah@gmail.com>
2020-12-14 20:58:06 +0700

    tugas baru

--- admin/latihan05/src/index.js
+++ admin/latihan05/src/index.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {PENDING,APPROVED, REJECTED, BANNED} from './Constant/Status';
+//import {PENDING,APPROVED, REJECTED, BANNED} from './Constant/Status';
 import Draw from './Constant';
+import * as status from './Constant/Status'
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-     
+     {status.APPROVED}
   <Draw nama="alfa" ItemClick={(e)=>{ console.log(e.target)}}/>
 
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-console.log(PENDING);
